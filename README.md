8177a6ec4fb143e9a9d8830e0fd7bf12dfdac05e
gzapodea <gzapodea@cisco.com>
2018-11-27 14:21:12 -0800

    updated for CL Live LATAM

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # DEVNET_2131_LA
+
+**DEVNET-2131**
+**NetDevOps Engineer Everyday Skills**
+
+This repo will include all the files needed for the DevNet Create 2018 session.
+
+Do you want to learn how to write simple ChatOps apps for IOS XE network devices?
+This session will explore a few IOS XE device programmability capabilities to help
+you create your first ChatOps application using NETCONF, RESTCONF, and Guest Shell.
+
+
+This repo will include all the code for the Cisco Live Cancun 2018 Session DEVNET-2131
+
---
+++ config.py
@@ -0,0 +1,19 @@
+
+# developed by Gabi Zapodeanu, TSA, GPO, Cisco Systems
+
+# This file contains the Spark Auth, ServiceNow Dev account info, router info
+
+WEBEX_TEAMS_URL = 'https://api.ciscospark.com/v1'
+WEBEX_TEAMS_AUTH = 'Bearer ' + 'Paste your Webex Teams Token here'
+WEBEX_TEAMS_SPACE = 'Config Change Alert'
+WEBEX_TEAMS_MEMBER = 'email'
+
+SNOW_URL = 'https://dev12345.service-now.com/api/now'
+SNOW_USER = 'user'
+SNOW_PASS = 'passw'
+SNOW_INSTANCE = 'dev12345'
+
+HOST = '10.10.20.48'
+USER = 'cisco'
+PASS = 'passw'
+PORT = '830'
